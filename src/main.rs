ae7340260e059afe425b252361e3f325b6a26494
Milan Buransky <milan.buransky@ims-evolve.com>
2015-11-18 13:45:15 +0000

    Redirection of the keys to the other machine.

--- src/main.rs
+++ src/main.rs
@@ -38,11 +38,48 @@ fn execute_message(msg : Message){
     };
 }
 
+fn send_message(stream : &mut TcpStream, msg : Message) -> Result<()>{
+    match msg{
+        Message::KeyPress(code) => {
+            let _ = try!(stream.write(&[1u8,code]));
+        }
+        _ => {}
+    }
+    Ok(())
+}
 
+
+#[cfg(target_os="macos")]
 fn run_master(stream : &mut TcpStream) -> Result<()>{
-    // loop{
-        // let ch = getch();
-    // }
+    let saved_term = match termios::tcgetattr(0){
+        Ok(t) => t,
+        Err(_) => return Err(Error::new(ErrorKind::Other,"Failed to create termios!"))
+    };
+    let mut term = saved_term;
+    // Unset canonical mode, so we get characters immediately
+    term.c_lflag.remove(termios::ICANON);
+    // Don't generate signals on Ctrl-C and friends
+    term.c_lflag.remove(termios::ISIG);
+    // Disable local echo
+    term.c_lflag.remove(termios::ECHO);
+    termios::tcsetattr(0, termios::TCSADRAIN, &term).unwrap();
+    println!("Press Ctrl-C to quit");
+    for byte in std::io::stdin().bytes() {
+        let byte = byte.unwrap();
+        if byte == 3 {
+            break;
+        } else {
+            println!("You pressed byte {}", byte);
+            send_message(stream,Message::KeyPress(byte));
+        }
+    }
+    println!("Goodbye!");
+    termios::tcsetattr(0, termios::TCSADRAIN, &saved_term).unwrap();
+    Ok(())
+}
+#[cfg(not(target_os="macos"))]
+fn run_master(stream : &mut TcpStream) -> Result<()>{
+    println!("Not supported on windows");
     Ok(())
 }
 
@@ -100,69 +137,39 @@ fn run_server(config : Config) -> Result<()> {
     Ok(())
 }
 
-#[cfg(target_os="macos")]
-fn run_key_test() -> Result<()>{
-    let saved_term = termios::tcgetattr(0).unwrap();
-    let mut term = saved_term;
-    // Unset canonical mode, so we get characters immediately
-    term.c_lflag.remove(termios::ICANON);
-    // Don't generate signals on Ctrl-C and friends
-    term.c_lflag.remove(termios::ISIG);
-    // Disable local echo
-    term.c_lflag.remove(termios::ECHO);
-    termios::tcsetattr(0, termios::TCSADRAIN, &term).unwrap();
-    println!("Press Ctrl-C to quit");
-    for byte in std::io::stdin().bytes() {
-        let byte = byte.unwrap();
-        if byte == 3 {
-            break;
-        } else {
-            println!("You pressed byte {}", byte);
-        }
-    }
-    println!("Goodbye!");
-    termios::tcsetattr(0, termios::TCSADRAIN, &saved_term).unwrap();
-    Ok(())
-}
-#[cfg(not(target_os="macos"))]
-fn run_key_test() ->Result<()> {
-    println!("Not supported on windows");
-    Ok(())
-}
-
 
 fn main() {
 
-    run_key_test().unwrap();
-
-    // let mut config = Config{
-    //     skip_client : false,
-    //     port: 24012,
-    //     localip : "127.0.0.1".to_owned(),
-    //     outsideip : "127.0.0.1".to_owned(),
-    //     master : true
-    // };
-    // {
-    //     let mut ap = ArgumentParser::new();
-    //     ap.set_description("Synchronise clipboard content between two computers.");
-    //     ap.refer(&mut config.skip_client)
-    //         .add_option(&["-s","--skip_client"],StoreTrue,"Skip connecting to client and create server right away.");
-    //     ap.refer(&mut config.port)
-    //         .add_option(&["-p","--port"],Store,"Port address");
-    //     ap.refer(&mut config.localip)
-    //         .add_option(&["-l","--local"],Store,"Local ip address");
-    //     ap.refer(&mut config.outsideip)
-    //         .add_option(&["-o","--outside"],Store,"Outside ip address");
-    //     ap.parse_args_or_exit();
-    // }
-    // println!("local: {}, outside: {}, port: {}",config.localip, config.outsideip, config.port );
-    // let _ = if config.skip_client{
-    //     println!("Skiping connecting to client");
-    //     run_server(config)
-    // }else {match try_run_client(config.clone()){
-    //     Err(_) => {
-    //         println!("Could not connect to server, creating own.");
-    //         run_server(config)},
-    //     _ => Ok(())
-    // }};
+    let mut config = Config{
+        skip_client : false,
+        port: 24012,
+        localip : "127.0.0.1".to_owned(),
+        outsideip : "127.0.0.1".to_owned(),
+        master : true
+    };
+    {
+        let mut ap = ArgumentParser::new();
+        ap.set_description("Synchronise clipboard content between two computers.");
+        ap.refer(&mut config.master)
+            .add_option(&["-m","--master"],StoreTrue,"Act as a master, this machine will redirect keyboard to the other one.").required();
+        ap.refer(&mut config.skip_client)
+            .add_option(&["-s","--skip_client"],StoreTrue,"Skip connecting to client and create server right away.");
+        ap.refer(&mut config.port)
+            .add_option(&["-p","--port"],Store,"Port address");
+        ap.refer(&mut config.localip)
+            .add_option(&["-l","--local"],Store,"Local ip address");
+        ap.refer(&mut config.outsideip)
+            .add_option(&["-o","--outside"],Store,"Outside ip address");
+        ap.parse_args_or_exit();
+    }
+    println!("local: {}, outside: {}, port: {}",config.localip, config.outsideip, config.port );
+    let _ = if config.skip_client{
+        println!("Skiping connecting to client");
+        run_server(config)
+    }else {match try_run_client(config.clone()){
+        Err(_) => {
+            println!("Could not connect to server, creating own.");
+            run_server(config)},
+        _ => Ok(())
+    }};
 }
