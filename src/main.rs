c6cfd8852d5d65b99a4cc587474d2acf20a57867
Milan Buransky <milan.buransky@ims-evolve.com>
2015-11-18 12:03:08 +0000

    Testing nix library.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,168 @@
+extern crate byteorder;
+extern crate argparse;
+#[cfg(target_os="macos")]
+extern crate nix;
+
+use std::net::{TcpListener,TcpStream};
+use std::io::{Error, ErrorKind, Result  };
+use std::thread;
+use byteorder::{ReadBytesExt, WriteBytesExt,  LittleEndian};
+use std::sync::mpsc::{Sender, channel};
+use std::time::Duration;
+use argparse::{ArgumentParser, Store,StoreTrue};
+use std::io::prelude::*;
+use std::io;
+
+#[cfg(target_os="macos")]
+use nix::sys::termios;
+
+
+#[derive(Clone)]
+struct Config {
+    skip_client : bool,
+    port : u16,
+    localip : String,
+    outsideip : String,
+    master : bool
+}
+
+enum Message{
+    Error,
+    KeyPress(u8)
+}
+
+fn execute_message(msg : Message){
+    match msg{
+        Message::KeyPress(code) => println!("received key: {}", code),
+        _ => println!("there is an error message!")
+    };
+}
+
+
+fn run_master(stream : &mut TcpStream) -> Result<()>{
+    // loop{
+        // let ch = getch();
+    // }
+    Ok(())
+}
+
+fn run_slave(stream : &mut TcpStream) -> Result<()>{
+    loop {
+        let code : u8 = try!(stream.read_u8());
+        let msg = match code{
+            1 => Message::KeyPress(try!(stream.read_u8())),
+            _ => Message::Error
+        };
+        execute_message(msg);
+    }
+    Ok(())
+}
+
+fn run_sync(stream : &mut TcpStream, config : Config) -> Result<()>{
+
+    if config.master {
+        run_master(stream)
+    }
+    else
+    {
+        run_slave(stream)
+    }
+}
+
+fn try_run_client(config : Config) -> Result<()>{
+    println!("Trying to connect to {}:{}",config.outsideip, config.port );
+    let mut stream = try!(TcpStream::connect((&config.outsideip as &str,config.port)));
+    run_sync(&mut stream,config)
+}
+
+
+fn run_server(config : Config) -> Result<()> {
+    println!("Creating server on {}:{}" , config.localip,config.port );
+    let listener = try!(TcpListener::bind((&config.localip as &str,config.port)));
+    println!("Waiting for new connections");
+
+    for stream in listener.incoming(){
+        match stream{
+            Ok(stream) => {
+                let cfg = config.clone();
+                thread::spawn(move||{
+                    println!("connected");
+                    run_sync(&mut stream.try_clone().unwrap(),cfg)
+                });
+            }
+            Err(e) => {
+                println!("Connection failed {}",e );
+            }
+        }
+    }
+
+    drop(listener);
+    Ok(())
+}
+
+#[cfg(target_os="macos")]
+fn run_key_test() -> Result<()>{
+    let saved_term = termios::tcgetattr(0).unwrap();
+    let mut term = saved_term;
+    // Unset canonical mode, so we get characters immediately
+    term.c_lflag.remove(termios::ICANON);
+    // Don't generate signals on Ctrl-C and friends
+    term.c_lflag.remove(termios::ISIG);
+    // Disable local echo
+    term.c_lflag.remove(termios::ECHO);
+    termios::tcsetattr(0, termios::TCSADRAIN, &term).unwrap();
+    println!("Press Ctrl-C to quit");
+    for byte in std::io::stdin().bytes() {
+        let byte = byte.unwrap();
+        if byte == 3 {
+            break;
+        } else {
+            println!("You pressed byte {}", byte);
+        }
+    }
+    println!("Goodbye!");
+    termios::tcsetattr(0, termios::TCSADRAIN, &saved_term).unwrap();
+    Ok(())
+}
+#[cfg(not(target_os="macos"))]
+fn run_key_test() ->Result<()> {
+    println!("Not supported on windows");
+    Ok(())
+}
+
+
 fn main() {
-    println!("Hello, world!");
+
+    run_key_test().unwrap();
+
+    // let mut config = Config{
+    //     skip_client : false,
+    //     port: 24012,
+    //     localip : "127.0.0.1".to_owned(),
+    //     outsideip : "127.0.0.1".to_owned(),
+    //     master : true
+    // };
+    // {
+    //     let mut ap = ArgumentParser::new();
+    //     ap.set_description("Synchronise clipboard content between two computers.");
+    //     ap.refer(&mut config.skip_client)
+    //         .add_option(&["-s","--skip_client"],StoreTrue,"Skip connecting to client and create server right away.");
+    //     ap.refer(&mut config.port)
+    //         .add_option(&["-p","--port"],Store,"Port address");
+    //     ap.refer(&mut config.localip)
+    //         .add_option(&["-l","--local"],Store,"Local ip address");
+    //     ap.refer(&mut config.outsideip)
+    //         .add_option(&["-o","--outside"],Store,"Outside ip address");
+    //     ap.parse_args_or_exit();
+    // }
+    // println!("local: {}, outside: {}, port: {}",config.localip, config.outsideip, config.port );
+    // let _ = if config.skip_client{
+    //     println!("Skiping connecting to client");
+    //     run_server(config)
+    // }else {match try_run_client(config.clone()){
+    //     Err(_) => {
+    //         println!("Could not connect to server, creating own.");
+    //         run_server(config)},
+    //     _ => Ok(())
+    // }};
 }
