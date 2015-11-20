afc99160b548734d6fe438615dbcd28bc818cfda
Milan Buransky <milan.buransky@ims-evolve.com>
2015-11-20 13:42:22 +0000

    Fix fox the keypress macro.

--- src/red_key/win.rs
+++ src/red_key/win.rs
@@ -20,37 +20,24 @@ macro_rules! keypress {
     ($ch:ident, $base : expr, $vk : expr, $from : expr, $to : expr) => {
         if($from <= $ch && $ch <= $to){
             let code = $ch as i32 - $base as i32 + $vk;
-            return vec!(Key::Press(delta),Key::Release(delta));
+            return vec!(Key::Press(code),Key::Release(code));
         }
-    }
+    };
     ($ch:ident, $base : expr, $vk : expr, $from : expr, $to : expr, $front : expr, $back : expr) => {
         if($from <= $ch && $ch <= $to){
             let code = $ch as i32 - $base as i32 + $vk;
-            return vec!($front,Key::Press(delta),Key::Release(delta),$back);
+            return vec!($front,Key::Press(code),Key::Release(code),$back);
         }
-    }
+    };
+    (shift => $ch:ident, $base : expr, $vk : expr, $from : expr, $to : expr) => {
+        keypress!($ch,$base,$vk,$from,$to,PressShift, ReleaseShift);
+    };
 }
 
 fn char2keys(ch : char) -> Vec<Key>{
-    keypress(ch,'A', 0x41, 'A', 'Z', PressShift, ReleaseShift);
-    keypress(ch,'a', 0x41, 'a', 'z');
-    keypress(ch,'0', 0x41, '0', '9');
-	// if 'A' <= ch && ch <= 'Z'{
-    //     let deltaA = ch as i32 - 'A' as i32;
-    //     return vec!(
-    //         PressShift,
-    //         Key::Press(deltaA+0x41),
-    //         Key::Release(deltaA+0x41),
-    //         ReleaseShift
-    //     );
-	// }
-	// if 'a' <= ch && ch <= 'z'{
-    //     let deltaA = ch as i32 - 'a' as i32;
-    //     return vec!(Key::Press(deltaA+0x41),Key::Release(deltaA+0x41));
-	// }
-    // if '0' <= ch && ch <= '9'{
-    //
-    // }
+    keypress!(shift => ch,'A', 0x41, 'A', 'Z');
+    keypress!(ch,'a', 0x41, 'a', 'z');
+    keypress!(ch,'0', 0x30, '0', '9');
     vec!()
 }
 
