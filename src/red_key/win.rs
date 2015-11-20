9869c868c3a3bf895141a529154fb68f15929e27
Milan Buransky <milan.buransky@ims-evolve.com>
2015-11-20 13:00:41 +0000

    Keypress logic is in a macor.

--- src/red_key/win.rs
+++ src/red_key/win.rs
@@ -16,22 +16,41 @@ enum Key {
 static PressShift : Key = Key::Press(winapi::VK_LSHIFT);
 static ReleaseShift : Key = Key::Release(winapi::VK_LSHIFT);
 
-
+macro_rules! keypress {
+    ($ch:ident, $base : expr, $vk : expr, $from : expr, $to : expr) => {
+        if($from <= $ch && $ch <= $to){
+            let code = $ch as i32 - $base as i32 + $vk;
+            return vec!(Key::Press(delta),Key::Release(delta));
+        }
+    }
+    ($ch:ident, $base : expr, $vk : expr, $from : expr, $to : expr, $front : expr, $back : expr) => {
+        if($from <= $ch && $ch <= $to){
+            let code = $ch as i32 - $base as i32 + $vk;
+            return vec!($front,Key::Press(delta),Key::Release(delta),$back);
+        }
+    }
+}
 
 fn char2keys(ch : char) -> Vec<Key>{
-	if 'A' <= ch && ch <= 'Z'{
-        let deltaA = ch as i32 - 'A' as i32;
-        return vec!(
-            PressShift,
-            Key::Press(deltaA+0x41),
-            Key::Release(deltaA+0x41),
-            ReleaseShift
-        );
-	}
-	if 'a' <= ch && ch <= 'z'{
-        let deltaA = ch as i32 - 'a' as i32;
-        return vec!(Key::Press(deltaA+0x41),Key::Release(deltaA+0x41));
-	}
+    keypress(ch,'A', 0x41, 'A', 'Z', PressShift, ReleaseShift);
+    keypress(ch,'a', 0x41, 'a', 'z');
+    keypress(ch,'0', 0x41, '0', '9');
+	// if 'A' <= ch && ch <= 'Z'{
+    //     let deltaA = ch as i32 - 'A' as i32;
+    //     return vec!(
+    //         PressShift,
+    //         Key::Press(deltaA+0x41),
+    //         Key::Release(deltaA+0x41),
+    //         ReleaseShift
+    //     );
+	// }
+	// if 'a' <= ch && ch <= 'z'{
+    //     let deltaA = ch as i32 - 'a' as i32;
+    //     return vec!(Key::Press(deltaA+0x41),Key::Release(deltaA+0x41));
+	// }
+    // if '0' <= ch && ch <= '9'{
+    //
+    // }
     vec!()
 }
 
